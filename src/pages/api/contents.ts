4eafc54dd6557ba5c2499ca699b18e5575a64a38
Luphia Chang <luphia@cafeca.io>
2022-12-02 16:48:53 +0800

    Fix content path

--- src/pages/api/contents.ts
+++ src/pages/api/contents.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import contents from '../../contents'
+import contents from '../../../contents'
 
 type Data = any
 
