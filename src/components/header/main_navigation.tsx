dfac8784ec1d9d2a1ca5be2b7a5d1caf6de487b0
Julian <julian890701@gmail.com>
2022-12-05 17:12:26 +0800

    Add subnavbar

--- src/components/header/main_navigation.tsx
+++ src/components/header/main_navigation.tsx
@@ -7,24 +7,83 @@ function MainNavigation() {
     <header className={styles.header}>
       <nav>
         <ul>
-          <li>
-            <Image
-              className={styles.logo}
-              src="/green.png"
-              alt="OMNI SPORTS Logo"
-              width={150}
-              height={70}
-            />
-          </li>
-          <li>
-            <a href="">活動專區</a>
-          </li>
-          <li>
-            <a href="">公告專區</a>
-          </li>
-          <li>
-            <a href="">運動統計</a>
-          </li>
+          <span className={styles.navl}>
+            <li>
+              <Image
+                className={styles.logo}
+                src="/green.png"
+                alt="OMNI SPORTS Logo"
+                width={150}
+                height={70}
+              />
+            </li>
+            <li>
+              <div className={styles.subnav}>
+                <button className={styles.subbtn}>活動專區</button>
+                <div className={styles.subnavcontent}>
+                  <a href="">講座</a>
+                  <a href="">體驗</a>
+                  <a href="">課程</a>
+                  <a href="">展覽</a>
+                  <a href="">其他</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className={styles.subnav}>
+                <button className={styles.subbtn}>公告專區</button>
+                <div className={styles.subnavcontent}>
+                  <a href="">最新消息</a>
+                  <a href="">系統公告</a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className={styles.subnav}>
+                <button className={styles.subbtn}>運動統計</button>
+                <div className={styles.subnavcontent}>
+                  <a href="">文本</a>
+                  <a href="">圖表</a>
+                </div>
+              </div>
+            </li>
+          </span>
+          <span className={styles.navr}>
+            <li>
+              <div className={styles.searchbar}>
+                <input id="search" type="search" placeholder="全站搜尋"></input>
+                <button>
+                  <Image
+                    src="/search.svg"
+                    alt="search btn"
+                    width={17}
+                    height={17}
+                  />
+                </button>
+              </div>
+            </li>
+            <li>
+              <button className={styles.menubtn}>網頁導覽</button>
+            </li>
+            <li>
+              <div className={styles.subnav}>
+                <button className={styles.menubtn}>
+                  Language
+                  <Image
+                    className={styles.menuarrow}
+                    src="/arrowdown.svg"
+                    alt="arrow"
+                    width={10}
+                    height={10}
+                  ></Image>
+                </button>
+                <div className={styles.subnavcontent}>
+                  <a href="">繁體中文</a>
+                  <a href="">English</a>
+                </div>
+              </div>
+            </li>
+          </span>
         </ul>
       </nav>
     </header>
