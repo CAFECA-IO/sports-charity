51ba2a59b48ae99ae305c22b26a5bb8c2b4770b7
Julian <julian890701@gmail.com>
2022-12-07 13:20:46 +0800

    Edit layout

--- src/components/flexslider.tsx
+++ src/components/flexslider.tsx
@@ -1,17 +1,13 @@
-import Image from "next/image";
-
 import styles from "../styles/flexslider.module.css";
 
 function Flexslider() {
   return (
     <div className={styles.flexslider}>
-      <Image
+      <img
         className={styles.picture}
         src="/placement_area.png"
         alt="2021 麥寮第一屆馬拉松活動"
-        width="1920"
-        height="800"
-      />
+      ></img>
     </div>
   );
 }
--- src/pages/others/navigation.tsx
+++ src/pages/others/navigation.tsx
@@ -1,5 +1,6 @@
-import styles from "../../styles/navigation.module.css";
 import Image from "next/image";
+
+import styles from "../../styles/navigation.module.css";
 
 function NavPage() {
   return (
