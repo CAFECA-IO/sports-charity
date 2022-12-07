8eec55eb6a79689bf8c74962439819e293e5056d
Julian <julian890701@gmail.com>
2022-12-07 13:21:39 +0800

    Add event page

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,4 +1,110 @@
+import Image from "next/image";
+
+import styles from "../../styles/eventindex.module.css";
+
 function AllEventsPage() {
-  return <div>All Events here</div>;
+  return (
+    <div>
+      <main className={styles.main}>
+        <span className={styles.banner}>
+          <nav className={styles.breadcrumb}>
+            <a href="/">首頁</a>{" "}
+            <Image
+              src="/arrowright.svg"
+              alt="arrow"
+              width={10}
+              height={10}
+            ></Image>
+            <a href="/others">活動專區</a>
+          </nav>
+          <h1>活動專區</h1>
+        </span>
+        <div className={styles.eventslist}>
+          <div className={styles.searchbar}>
+            <input id="search" type="search" placeholder="活動搜尋"></input>
+            <button>
+              <Image
+                src="/search.svg"
+                alt="search btn"
+                width={17}
+                height={17}
+              />
+            </button>
+          </div>
+          <div className={styles.flexbox}>
+            <a href="" className={styles.ticket}>
+              <div className={styles.imgbox}>
+                <img src="/Image-1.png" alt="event1"></img>
+              </div>
+              <div className={styles.textbox}>
+                <h2>LINE FRIENDS 音樂路跑</h2>
+                <p className={styles.intro}>
+                  這個路跑有配樂！2022.9.7 全球首屆「LINE FRIENDS
+                  音樂路跑」登陸台北路跑聯名商品全台獨家推出，等你來報名。
+                </p>
+              </div>
+            </a>
+            <a href="" className={styles.ticket}>
+              <div className={styles.imgbox}>
+                <img src="/Image-2.png" alt="event2"></img>
+              </div>
+              <div className={styles.textbox}>
+                <h2>運動 x 科技 x 設計 系列講座</h2>
+                <p className={styles.intro}>
+                  本次座談特地邀請實際在運動市場上應用科技的企業代表與專家們，與設研院一同探討科技如何利用設計串聯運動產業，讓大家可以sport
+                  everywhere！
+                </p>
+              </div>
+            </a>
+            <a href="" className={styles.ticket}>
+              <div className={styles.imgbox}>
+                <img src="/Image-3.png" alt=""></img>
+              </div>
+              <div className={styles.textbox}>
+                <h2>台北國際運動及健身展</h2>
+                <p className={styles.intro}>
+                  2023年3月22-25日 台北南港展覽館2館(台北市南港區經貿二路2號)
+                </p>
+              </div>
+            </a>
+            <a href="" className={styles.ticket}>
+              <div className={styles.imgbox}>
+                <img src="/Image-4.png" alt=""></img>
+              </div>
+              <div className={styles.textbox}>
+                <h2>運動星球能量營</h2>
+                <p className={styles.intro}>
+                  2022夏令營終於來啦~~爸爸媽媽照過來
+                  運動星球能量營【三鶯最強團隊X新北最強營隊】
+                </p>
+              </div>
+            </a>
+            <a href="" className={styles.ticket}>
+              <div className={styles.imgbox}>
+                <img src="/Image-5.png" alt=""></img>
+              </div>
+              <div className={styles.textbox}>
+                <h2>向海致敬-區域性水域體驗活動</h2>
+                <p className={styles.intro}>
+                  西灣學院運動與健康教育中心攜手學務處體育與衛生保健組，共同推動國家海洋政策，帶動全民海洋教育，特辦理旨揭體驗活動。
+                </p>
+              </div>
+            </a>
+            <a href="" className={styles.ticket}>
+              <div className={styles.imgbox}>
+                <img src="/Image-6.png" alt=""></img>
+              </div>
+              <div className={styles.textbox}>
+                <h2>黃阿瑪要跑不跑的路跑</h2>
+                <p className={styles.intro}>
+                  黃阿瑪的後宮生活正式授權，首次大型戶外運動活動號召百萬瑪民動起來，邁開步伐，喵速衝刺！
+                </p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 export default AllEventsPage;
